330caea0f9e038eaa964d1b9ce037212998d44a7
hiencoder <nguyenhien1989hd@gmail.com>
2018-04-18 07:30:05 +0700

    Commit icon

---
+++ app/src/main/java/vn/edu/imic/hairrstylesticker/view/HomeActivity.java
@@ -0,0 +1,16 @@
+package vn.edu.imic.hairrstylesticker.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.edu.imic.hairrstylesticker.R;
+
+public class HomeActivity extends AppCompatActivity {
+    /**/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
--- app/src/main/java/vn/edu/imic/hairrstylesticker/view/MainActivity.java
+++ app/src/main/java/vn/edu/imic/hairrstylesticker/view/MainActivity.java
@@ -2,14 +2,36 @@ package vn.edu.imic.hairrstylesticker.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import vn.edu.imic.hairrstylesticker.R;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btn_open_gallery)
+    ImageView btnOpenGallery;
 
+    @BindView(R.id.btn_open_camera)
+    ImageView btnOpenCamera;
+
+    @BindView(R.id.btn_more_app)
+    ImageView btnOpenMoreApp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_open_gallery, R.id.btn_open_camera, R.id.btn_more_app})
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_open_gallery:
+
+                break;
+        }
     }
 }
