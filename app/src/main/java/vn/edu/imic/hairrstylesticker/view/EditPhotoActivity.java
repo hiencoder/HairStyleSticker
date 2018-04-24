10d496bbbddb51bfc6f519359e086935e9d5e907
hiennv1 <hiennv1@runsystem.net>
2018-04-24 17:03:51 +0700

    Commit show style

--- app/src/main/java/vn/edu/imic/hairrstylesticker/view/EditPhotoActivity.java
+++ app/src/main/java/vn/edu/imic/hairrstylesticker/view/EditPhotoActivity.java
@@ -1,25 +1,31 @@
 package vn.edu.imic.hairrstylesticker.view;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+
 import android.util.Log;
+import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 import vn.edu.imic.hairrstylesticker.R;
-import vn.edu.imic.hairrstylesticker.network.Style;
-import vn.edu.imic.hairrstylesticker.view.fragment.FragmentStyle;
+import vn.edu.imic.hairrstylesticker.utils.Logger;
 
-public class EditPhotoActivity extends AppCompatActivity implements FragmentStyle.SendStyle {
+public class EditPhotoActivity extends AppCompatActivity {
     private static final String TAG = EditPhotoActivity.class.getSimpleName();
     private Intent intent;
     private Uri uri;
@@ -44,6 +50,8 @@ public class EditPhotoActivity extends AppCompatActivity implements FragmentStyl
     ImageView imgChooseStyle;
 
     /*Các style được chọn*/
+    @BindView(R.id.rl_styles)
+    RelativeLayout rlStyles;
     @BindView(R.id.btn_hair)
     RelativeLayout btnHair;
     @BindView(R.id.btn_mus)
@@ -60,6 +68,8 @@ public class EditPhotoActivity extends AppCompatActivity implements FragmentStyl
     RelativeLayout btnSuite;
 
     /*Màu sắc để color filter*/
+    @BindView(R.id.rl_color)
+    RelativeLayout rlColor;
     @BindView(R.id.ib_color0)
     ImageButton btnColor0;
     @BindView(R.id.ib_color1)
@@ -104,124 +114,94 @@ public class EditPhotoActivity extends AppCompatActivity implements FragmentStyl
     ImageButton btnColor20;
     @BindView(R.id.ib_color21)
     ImageButton btnColor21;
+    @BindView(R.id.ib_color22)
+    ImageButton btnColor22;
+    @BindView(R.id.ib_color23)
+    ImageButton btnColor23;
+    @BindView(R.id.ib_color24)
+    ImageButton btnColor24;
+    @BindView(R.id.ib_color25)
+    ImageButton btnColor25;
+    @BindView(R.id.ib_color26)
+    ImageButton btnColor26;
+    @BindView(R.id.ib_color27)
+    ImageButton btnColor27;
+    @BindView(R.id.ib_color28)
+    ImageButton btnColor28;
+    @BindView(R.id.ib_color29)
+    ImageButton btnColor29;
+    @BindView(R.id.ib_color30)
+    ImageButton btnColor30;
+    @BindView(R.id.ib_color31)
+    ImageButton btnColor31;
+
+    /*Cac imageview click*/
+    @BindView(R.id.btn_close_hair)
+    ImageView btnCloseHair;
+    @BindView(R.id.btn_flip)
+    ImageView btnRotate;
+    @BindView(R.id.btn_save_hair)
+    ImageView btnSaveHair;
 
     /*Lấy ảnh từ drawable*/
-    private FragmentManager fmEditPhoto;
+    @BindView(R.id.hsv_item)
+    HorizontalScrollView hsvItem;
 
+    /*Button edit*/
+    @BindView(R.id.btn_edit)
+    TextView btnEdit;
+    /*Biến số lượng item của */
+    private int itemSize;
+    //Tag title style
+    private String tagStyle;
+    //Hien thi tag item
+    private String tagItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_photo_3);
         unbinder = ButterKnife.bind(this);
+        setTintForButton();
         intent = getIntent();
         initData();
     }
 
-    /*init data*/
-    private void initData() {
-        result = CropImage.getActivityResult(intent);
-        if (result != null) {
-            Log.d(TAG, "initData: " + result.getUri());
-            imgCrop.setImageURI(result.getUri());
-        }
-        //imgCrop.setImageURI(result.getUri());
-
-        /*Đổ fragment*/
-/*
-        fmEditPhoto = getSupportFragmentManager();
-        FragmentTransaction ftStyle = fmEditPhoto.beginTransaction();
-        ftStyle.add(R.id.frame_content,new FragmentStyle()).commitAllowingStateLoss();
-*/
+    /*Set tint for button*/
+    private void setTintForButton() {
+        btnColor0.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor1.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor2.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor3.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor4.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor5.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor6.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor7.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor8.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor9.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor10.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor11.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor12.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor13.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor14.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor15.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor16.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor17.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor18.setColorFilter(Color.parseColor("#FF8000"), PorterDuff.Mode.MULTIPLY);
+        btnColor19.setColorFilter(Color.parseColor("#977961"), PorterDuff.Mode.MULTIPLY);
+        btnColor20.setColorFilter(Color.parseColor("#7D9EC0"), PorterDuff.Mode.MULTIPLY);
+        btnColor21.setColorFilter(Color.parseColor("#AB82FF"), PorterDuff.Mode.MULTIPLY);
+        btnColor22.setColorFilter(Color.parseColor("#FFB90F"), PorterDuff.Mode.MULTIPLY);
+        btnColor23.setColorFilter(Color.parseColor("#FFF5E1"), PorterDuff.Mode.MULTIPLY);
+        btnColor24.setColorFilter(Color.parseColor("#4876FF"), PorterDuff.Mode.MULTIPLY);
+        btnColor25.setColorFilter(Color.parseColor("#FFA07A"), PorterDuff.Mode.MULTIPLY);
+        btnColor26.setColorFilter(Color.parseColor("#BCD2EE"), PorterDuff.Mode.MULTIPLY);
+        btnColor27.setColorFilter(Color.parseColor("#7171C6"), PorterDuff.Mode.MULTIPLY);
+        btnColor28.setColorFilter(Color.parseColor("#C1FFC1"), PorterDuff.Mode.MULTIPLY);
+        btnColor29.setColorFilter(Color.parseColor("#B7A69E"), PorterDuff.Mode.MULTIPLY);
+        btnColor30.setColorFilter(Color.parseColor("#BCEE68"), PorterDuff.Mode.MULTIPLY);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unbinder.unbind();
-    }
-
-    @Override
-    public void sendStyle(Style style) {
-
-    }
-
-    /*
-        public void showScrollViewWithIndex(int i) {
-            VisibilityThings(false);
-            this.ag.setVisibility(4);
-            this.ah.setVisibility(0);
-            this.V.setColorFilter(-1, Mode.MULTIPLY);
-            removeScrollview();
-            this.af.setVisibility(0);
-            HorizontalScrollView horizontalScrollView = (HorizontalScrollView) findViewById(R.id.scrollviewForItem);
-            horizontalScrollView.setVisibility(0);
-            float f = getResources().getDisplayMetrics().density * 50.0f;
-            View linearLayout = new LinearLayout(this);
-            linearLayout.setOrientation(0);
-            LayoutParams layoutParams = new LinearLayout.LayoutParams(-1, (int) f);
-            linearLayout.setBackgroundColor(0);
-            linearLayout.setLayoutParams(layoutParams);
-            horizontalScrollView.addView(linearLayout);
-            switch (i) {
-                case 1:
-                    this.mImageHeight = this.j;
-                    this.mImageWidth = this.i;
-                    this.ac = 42;
-                    this.ab = "img";
-                    break;
-                case 2:
-                    this.mImageHeight = this.l;
-                    this.mImageWidth = this.k;
-                    this.ac = 31;
-                    this.ab = "mustachi";
-                    break;
-                case 3:
-                    this.mImageHeight = this.n;
-                    this.mImageWidth = this.m;
-                    this.ac = 14;
-                    this.ab = "mus";
-                    break;
-                case 4:
-                    this.mImageHeight = this.p;
-                    this.mImageWidth = this.o;
-                    this.ac = 14;
-                    this.ab = "glass";
-                    break;
-                case 5:
-                    this.mImageHeight = this.r;
-                    this.mImageWidth = this.q;
-                    this.ac = 19;
-                    this.ab = "tatto";
-                    break;
-                case 6:
-                    this.mImageHeight = this.t;
-                    this.mImageWidth = this.t;
-                    this.ac = 21;
-                    this.ab = "cap";
-                    break;
-            }
-            for (int i2 = 1; i2 <= this.ac; i2++) {
-                int identifier = this.W.getResources().getIdentifier(this.ab + i2, "drawable", this.W.getPackageName());
-                View imageButton = new ImageButton(this);
-                imageButton.setLayoutParams(new LinearLayout.LayoutParams((int) f, (int) f));
-                imageButton.setScaleType(ScaleType.CENTER_CROP);
-                imageButton.setBackgroundColor(Color.parseColor("#00ffffff"));
-                imageButton.setTag(this.ab + "_" + i2);
-                imageButton.setImageResource(identifier);
-                imageButton.setOnClickListener(new OnClickListener(this) {
-                    final */
-/* synthetic *//*
- Imageselection b;
-
-                public void onClick(View view) {
-                    this.b.addSelectedImage(view, i2);
-                }
-            });
-            linearLayout.addView(imageButton);
-        }
-    }
-*/
-    /*public void SetTintforButton() {
+        /*public void SetTintforButton() {
         ((ImageButton) findViewById(R.id.color1)).setColorFilter(Color.parseColor("#FF8000"), Mode.MULTIPLY);
         ((ImageButton) findViewById(R.id.color2)).setColorFilter(Color.parseColor("#F0E68C"), Mode.MULTIPLY);
         ((ImageButton) findViewById(R.id.color3)).setColorFilter(Color.parseColor("#00EEEE"), Mode.MULTIPLY);
@@ -240,19 +220,133 @@ public class EditPhotoActivity extends AppCompatActivity implements FragmentStyl
         ((ImageButton) findViewById(R.id.color16)).setColorFilter(Color.parseColor("#71635A"), Mode.MULTIPLY);
         ((ImageButton) findViewById(R.id.color17)).setColorFilter(Color.parseColor("#D8BFD8"), Mode.MULTIPLY);
         ((ImageButton) findViewById(R.id.color18)).setColorFilter(Color.parseColor("#71C671"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color19)).setColorFilter(Color.parseColor("#977961"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color20)).setColorFilter(Color.parseColor("#7D9EC0"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color21)).setColorFilter(Color.parseColor("#AB82FF"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color22)).setColorFilter(Color.parseColor("#FFB90F"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color23)).setColorFilter(Color.parseColor("#FFF5E1"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color24)).setColorFilter(Color.parseColor("#4876FF"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color25)).setColorFilter(Color.parseColor("#FFA07A"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color26)).setColorFilter(Color.parseColor("#BCD2EE"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color27)).setColorFilter(Color.parseColor("#7171C6"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color28)).setColorFilter(Color.parseColor("#C1FFC1"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color29)).setColorFilter(Color.parseColor("#B7A69E"), Mode.MULTIPLY);
-        ((ImageButton) findViewById(R.id.color30)).setColorFilter(Color.parseColor("#BCEE68"), Mode.MULTIPLY);
     }
 */
+
+    /*init data*/
+    private void initData() {
+        result = CropImage.getActivityResult(intent);
+        if (result != null) {
+            Log.d(TAG, "initData: " + result.getUri());
+            imgCrop.setImageURI(result.getUri());
+        }
+
+    }
+
+    @OnClick({R.id.btn_hair,R.id.btn_beard,R.id.btn_mus,R.id.btn_glasses,R.id.btn_tattoo,R.id.btn_cap,R.id.btn_suite,
+    R.id.btn_edit,R.id.btn_close_hair,R.id.btn_flip})
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_hair:
+                showHorizontalItem(1);
+                break;
+            case R.id.btn_beard:
+                showHorizontalItem(2);
+                break;
+            case R.id.btn_mus:
+                showHorizontalItem(3);
+                break;
+            case R.id.btn_glasses:
+                showHorizontalItem(4);
+                break;
+            case R.id.btn_tattoo:
+                showHorizontalItem(5);
+                break;
+            case R.id.btn_cap:
+                showHorizontalItem(6);
+                break;
+            case R.id.btn_suite:
+                break;
+        }
+    }
+
+
+    /*Show hair item*/
+    private void showHorizontalItem(int index) {
+        /*Get item type hair*/
+        //Ẩm Relative style
+        rlStyles.setVisibility(View.INVISIBLE);
+        //Hien thi layout chua horizontal
+        rlColor.setVisibility(View.VISIBLE);
+        hsvItem.setVisibility(View.VISIBLE);
+
+        //Hien thi cac imageview
+        btnCloseHair.setVisibility(View.VISIBLE);
+        btnRotate.setVisibility(View.VISIBLE);
+        btnSaveHair.setVisibility(View.VISIBLE);
+        /*Remove all item*/
+        hsvItem.removeAllViews();
+        //Tạo LinearLayout horizontal
+        LinearLayout llItemStyle = new LinearLayout(this);
+        llItemStyle.setOrientation(LinearLayout.HORIZONTAL);
+        float f = getResources().getDisplayMetrics().density * 50.0f;
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int) f);
+        llItemStyle.setLayoutParams(layoutParams);
+
+        switch (index){
+            case 1:
+                itemSize = 31;
+                tagStyle = "img";
+                tagItem = "Hair";
+                break;
+            case 2:
+                itemSize = 31;
+                tagStyle = "mustachi";
+                tagItem = "Beard";
+                break;
+            case 3:
+                itemSize = 14;
+                tagStyle = "mus";
+                tagItem = "Mustachi";
+                break;
+            case 4:
+                itemSize = 12;
+                tagStyle = "glass";
+                tagItem = "Glass";
+                break;
+            case 5:
+                itemSize = 19;
+                tagStyle = "tatto";
+                tagItem = "Tattoo";
+                break;
+            case 6:
+                itemSize = 19;
+                tagStyle = "cap";
+                tagItem = "Caps";
+                break;
+            case 7:
+                break;
+        }
+        itemSize = 31;
+        for (int i = 1; i < itemSize; i++) {
+            /*Lay anh tu drawable*/
+            int identifier = getResources().getIdentifier(tagStyle + i,"drawable",getPackageName());
+            final ImageButton imageButton = new ImageButton(this);
+            imageButton.setLayoutParams(new LinearLayout.LayoutParams((int) f, (int) f));
+            imageButton.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageButton.setBackgroundColor(Color.parseColor("#00ffffff"));
+            imageButton.setTag(tagItem + "_" + i);
+            imageButton.setImageResource(identifier);
+
+
+            /*Su kien click cho button*/
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Logger.i(tagItem, (String) imageButton.getTag());
+                }
+            });
+            llItemStyle.addView(imageButton);
+        }
+        hsvItem.addView(llItemStyle);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+
 
 }
