41f3bb5795da4a53879dde1a551cbe5d66c09a94
hiencoder <nguyenhien1989hd@gmail.com>
2018-05-07 08:08:38 +0700

    Commit edit

--- app/src/main/java/vn/edu/imic/hairrstylesticker/view/CropImageActivity.java
+++ app/src/main/java/vn/edu/imic/hairrstylesticker/view/CropImageActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,11 +14,11 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.edmodo.cropper.CropImageView;
 
@@ -43,7 +44,7 @@ import vn.edu.imic.hairrstylesticker.utils.Logger;
  * Created by MyPC on 29/04/2018.
  */
 
-public class CropImageActivity extends AppCompatActivity{
+public class CropImageActivity extends AppCompatActivity {
     private static final String TAG = CropImageActivity.class.getSimpleName();
     @BindView(R.id.btn_camera)
     ImageView btnCamera;
@@ -85,6 +86,9 @@ public class CropImageActivity extends AppCompatActivity{
 
     private Unbinder unbinder;
 
+    //Biến góc quay
+    private int rotateDegree = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,12 +96,17 @@ public class CropImageActivity extends AppCompatActivity{
         unbinder = ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.btn_camera,R.id.btn_gallery,
-    R.id.btn_free_size,R.id.btn_square,R.id.btn_rotate,R.id.btn_done})
-    public void onClick(View v){
-        switch (v.getId()){
+    @OnClick({R.id.btn_camera, R.id.btn_gallery,
+            R.id.btn_free_size, R.id.btn_square, R.id.btn_rotate, R.id.btn_done})
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.btn_camera:
-                openCamera();
+                /*Kiểm tra điều kiện để open camera*/
+                if (Build.VERSION.SDK_INT <= 19) {
+                    openCameraWithSdk();
+                } else {
+                    openCamera();
+                }
                 break;
             case R.id.btn_gallery:
                 openGallery();
@@ -117,33 +126,55 @@ public class CropImageActivity extends AppCompatActivity{
         }
     }
 
+    /*Open camera vs sdk nhỏ hơn 19*/
+    private void openCameraWithSdk() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, Const.REQUEST_OPEN_CAMERA);
+    }
+
 
     /*Handle save*/
     private void handleSave() {
-
+        try {
+            Uri saveBitmap = saveBitmap(civImage.getCroppedImage());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     //http://www.appwallettech.com/appwalletftp/StartAdFile.xml
     /*Handle rotate*/
     private void handleRotate() {
+        //Tăng góc quay 90 độ
+        rotateDegree += 90;
+        if (rotateDegree == 360){
+            rotateDegree = 0;
+        }
+        civImage.rotateImage(rotateDegree);
 
     }
 
     /*Handle square size*/
     private void handleSquareSize() {
         civImage.setFixedAspectRatio(true);
+        //Set backround cho button
+        btnSquare.setBackgroundColor(Color.parseColor("#a5212121"));
+        btnFreeSize.setBackgroundColor(Color.parseColor("#ffffff"));
     }
 
     /*Handle Freesize*/
     private void handleFreeSize() {
         civImage.setFixedAspectRatio(false);
+        //Set background cho button
+        btnFreeSize.setBackgroundColor(Color.parseColor("#a5212121"));
+        btnSquare.setBackgroundColor(Color.parseColor("#ffffff"));
     }
 
     /*Open gallery*/
     private void openGallery() {
         //Tao intent voi action mo gallery
         Intent iGallery = new Intent(Intent.ACTION_PICK);
-        iGallery.setDataAndType(Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath()),"image/*");
+        iGallery.setDataAndType(Uri.parse(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath()), "image/*");
         startActivityForResult(iGallery, Const.REQUEST_OPEN_GALLERY);
     }
 
@@ -151,50 +182,38 @@ public class CropImageActivity extends AppCompatActivity{
     private void openCamera() {
         Intent iCamera = new Intent("android.media.action.IMAGE_CAPTURE");
         String dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File file = Environment.getExternalStorageState().equals("mounted")?
-                new File(Environment.getExternalStorageDirectory(), FOLDER_NAME +
-                        File.separator + dateFormat + ".jpeg")
-                : new File(getCacheDir(),FOLDER_NAME + File.separator + dateFormat + ".jpeg");
-        File file2 = new File(Environment.getExternalStorageDirectory(),System.currentTimeMillis() + ".jpeg");
-/*
         File file = Environment.getExternalStorageState().equals("mounted") ?
                 new File(Environment.getExternalStorageDirectory(), FOLDER_NAME +
-                        File.separator + format + ".jpeg")
-                : new File(getCacheDir(), FOLDER_NAME + File.separator + format + ".jpeg");
+                        File.separator + dateFormat + ".jpeg")
+                : new File(getCacheDir(), FOLDER_NAME + File.separator + dateFormat + ".jpeg");
         File file2 = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpeg");
-*/
 
-        if (file != null){
-            iCamera.putExtra("output",Uri.fromFile(file2));
+        if (file != null) {
+            iCamera.putExtra("output", Uri.fromFile(file2));
             uriSelected = Uri.fromFile(file2);
-            Log.d(TAG, "openCamera: " + uriSelected);
-            startActivityForResult(iCamera,Const.REQUEST_OPEN_CAMERA);
+            startActivityForResult(iCamera, Const.REQUEST_OPEN_CAMERA);
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data != null){
-            if (requestCode == Const.REQUEST_OPEN_GALLERY && resultCode == Activity.RESULT_OK){
-                //Neu la mo gallery
+        if (requestCode == Const.REQUEST_OPEN_CAMERA && resultCode == Activity.RESULT_OK) {
+            if (Build.VERSION.SDK_INT <= 19) {
                 cropImage(data.getData());
-            }else if (requestCode == Const.REQUEST_OPEN_CAMERA && resultCode == Activity.RESULT_OK){
-                if (Build.VERSION.SDK_INT <= 19){
-                    cropImage(data.getData());
-                    Logger.d(TAG, "UriSelected19: " + data.getData());
-                }else{
-                    uriSelected = Uri.parse(data.getStringExtra("output"));
-                    Log.d(TAG, "onActivityResult: " + uriSelected);
-                    cropImage(uriSelected);
-                }
+            } else if (Build.VERSION.SDK_INT >= 20 && requestCode == Const.REQUEST_OPEN_CAMERA
+                    && resultCode == Activity.RESULT_OK) {
+                cropImage(uriSelected);
             }
+        } else if (requestCode == Const.REQUEST_OPEN_GALLERY && resultCode == Activity.RESULT_OK) {
+            cropImage(data.getData());
         }
     }
 
     /*Crop Image*/
     private void cropImage(Uri data) {
-        if (data != null){
+        Logger.i(TAG, data.toString());
+        if (data != null) {
             InputStream openInputStream;
             try {
                 openInputStream = getContentResolver().openInputStream(data);
@@ -202,30 +221,37 @@ public class CropImageActivity extends AppCompatActivity{
                 e.printStackTrace();
                 openInputStream = null;
             }
-            bitmapSelected = BitmapFactory.decodeStream(openInputStream);
-            //Hien thi layout chua bitmap
-            rlCropActivity.setVisibility(View.VISIBLE);
-            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-            bitmapSelected = resizeImageToNewSize(bitmapSelected,displayMetrics.widthPixels,
-                    displayMetrics.heightPixels - ((int) (getResources().getDisplayMetrics().density*50.0f)));
-            if (this.bitmapSelected != null){
+            try {
+                bitmapSelected = BitmapFactory.decodeStream(openInputStream);
+                //Hien thi layout chua bitmap
+                rlCropActivity.setVisibility(View.VISIBLE);
+                DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+                bitmapSelected = resizeImageToNewSize(bitmapSelected, displayMetrics.widthPixels,
+                        displayMetrics.heightPixels - ((int) (getResources().getDisplayMetrics().density * 50.0f)));
+                if (this.bitmapSelected != null) {
                 /*Set kich thuoc cho CropImageView*/
-                civImage.getLayoutParams().width = bitmapSelected.getWidth();
-                civImage.getLayoutParams().height = bitmapSelected.getHeight();
-                Log.d(TAG, "cropImage: Width" + bitmapSelected.getWidth()
-                        + "\nHeight: " + bitmapSelected.getHeight());
-                //Hien thi bitmap len CropImageView
-                civImage.setImageBitmap(bitmapSelected);
-                civImage.setAspectRatio(5,5);
+                    civImage.getLayoutParams().width = bitmapSelected.getWidth();
+                    civImage.getLayoutParams().height = bitmapSelected.getHeight();
+                    Logger.d(TAG, "cropImage: Width: " + bitmapSelected.getWidth()
+                            + "\nHeight: " + bitmapSelected.getHeight());
+                    //Hien thi bitmap len CropImageView
+                    civImage.setImageBitmap(bitmapSelected);
+                    civImage.setAspectRatio(5, 5);
+                    return;
+                }
+                return;
+            }catch (Exception ex2){
+                Toast.makeText(this, "Please select other image!", Toast.LENGTH_SHORT).show();
+                return;
             }
         }
+        Toast.makeText(this, "Please select other image!", Toast.LENGTH_SHORT).show();
     }
 
     /**
-     *
      * @param bm
-     * @param w : chieu rong ban dau
-     * @param h chieu cao ban dau
+     * @param w  : chieu rong ban dau
+     * @param h  chieu cao ban dau
      * @return
      */
     private Bitmap resizeImageToNewSize(Bitmap bm, int w, int h) {
@@ -233,10 +259,10 @@ public class CropImageActivity extends AppCompatActivity{
         int height = bm.getHeight();
         float f = (float) w; // new size(width)
         float f2 = (float) h; // new size(height)
-        if (!(height == h) && (width == w)){
-            f2 = ((float) w)/((float) width);
+        if (!(height == h) && (width == w)) {
+            f2 = ((float) w) / ((float) width);
             f = ((float) h) / ((float) height);
-            if (f2 >= f){
+            if (f2 >= f) {
                 f2 = f;
             }
             f = ((float) width) * f2;
@@ -245,7 +271,7 @@ public class CropImageActivity extends AppCompatActivity{
         /*Tao mot bitmap duoc chia ti le tu mot bitmap co truoc
         * Neu chieu cao va chieu rong duoc xac dinh giong voi kich thuoc hien
         * tai cua bitmap thi bitmap nguon duoc tra ve va khong co bitmap moi duoc tao ra.*/
-        return Bitmap.createScaledBitmap(bm,(int) f,(int) f2,true);
+        return Bitmap.createScaledBitmap(bm, (int) f, (int) f2, true);
     }
 
     /*Phuong thuc luu Bitmap*/
@@ -254,16 +280,16 @@ public class CropImageActivity extends AppCompatActivity{
         ContentValues values;
         Throwable th;
         Uri uri = null;
-        if (bm == null || bm.isRecycled()){
+        if (bm == null || bm.isRecycled()) {
             return null;
         }
         File file = new File(Environment.getExternalStorageDirectory() + "/TempImage");
-        if (!file.exists()){
+        if (!file.exists()) {
             file.mkdir();
         }
         int nextInt = new Random().nextInt(1000);
-        File file2 = new File(file,String.format("%s_%d.png", new Object[]{"TempImage",Integer.valueOf(nextInt)}));
-        if (file2.exists() && file2.delete()){
+        File file2 = new File(file, String.format("%s_%d.png", new Object[]{"TempImage", Integer.valueOf(nextInt)}));
+        if (file2.exists() && file2.delete()) {
             try {
                 file2.createNewFile();
             } catch (IOException e) {
@@ -273,64 +299,79 @@ public class CropImageActivity extends AppCompatActivity{
         try {
             outputStream = new FileOutputStream(file2);
             try {
-                bm.compress(Bitmap.CompressFormat.PNG,100,outputStream);
-            }catch (Exception ex2){
+                bm.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            } catch (Exception ex2) {
                 Object obj = outputStream;
-                if (uri != null){
+                if (uri != null) {
                 }
                 values = new ContentValues(3);
-                values.put("title","TempImage");
-                values.put("mime_type","image/jpeg");
-                values.put("_data",file2.getAbsolutePath());
+                values.put("title", "TempImage");
+                values.put("mime_type", "image/jpeg");
+                values.put("_data", file2.getAbsolutePath());
                 uri = Uri.fromFile(file2.getAbsoluteFile());
-                getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+                getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
                 return uri;
-            }catch (Throwable th2){
+            } catch (Throwable th2) {
                 th = th2;
-                if (outputStream == null){
+                if (outputStream == null) {
                     throw th;
                 }
                 throw th;
             }
 
         } catch (Exception e3) {
-            if (uri != null){
+            if (uri != null) {
             }
             values = new ContentValues(3);
-            values.put("title","TempImage");
-            values.put("mime_type","image/jpeg");
-            values.put("_data",file2.getAbsolutePath());
+            values.put("title", "TempImage");
+            values.put("mime_type", "image/jpeg");
+            values.put("_data", file2.getAbsolutePath());
             uri = Uri.fromFile(file2.getAbsoluteFile());
-            getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+            getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
             return uri;
-        }catch (Throwable th3){
+        } catch (Throwable th3) {
             Throwable th4 = th3;
             outputStream = null;
             th = th4;
-            if (outputStream == null){
+            if (outputStream == null) {
                 throw th4;
             }
             throw th;
         }
         return uri;
     }
+
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         /*Lay ra uri*/
-        uriSelected = savedInstanceState.getParcelable(Const.KEY_PHOTO_URI);
+        uriSelected = (Uri) savedInstanceState.getParcelable(Const.KEY_PHOTO_URI);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         /*Put uri vao*/
-        outState.putParcelable(Const.KEY_PHOTO_URI,uriSelected);
+        outState.putParcelable(Const.KEY_PHOTO_URI, uriSelected);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         //unbinder.unbind();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Runtime.getRuntime().gc();
+        if (bitmapSelected != null){
+            bitmapSelected.recycle();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
     }
 }
